efd4b82e96b7421ced2880f3cfa0d9786fe4c9a6
Daniel Ferreira Sousa <danielferro01234@gmail.com>
2025-09-03 18:59:23 -0300

    projeto funcionando

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//variavel para guardar os nomes dos amigos
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome_Amigos = document.getElementById("amigo");
+    if (nome_Amigos.value === ''){
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nome_Amigos.value);
+        document.getElementById("amigo").value = '';
+        listaAmigos();
+    }
+}
+
+function listaAmigos(){
+    if (amigos.length != 0){
+        lista = document.getElementById("listaAmigos");
+        lista.innerHTML = "";
+        amigos.forEach(function(amigos){
+            let li = document.createElement("li");
+            li.textContent = amigos;
+            lista.appendChild(li);
+        });
+    }
+}
+
+function sortearAmigo(){
+    tamanho = amigos.length;
+    sorteado = Math.floor(Math.random() * tamanho)
+
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    resultado.innerHTML = amigos[sorteado];
+}
